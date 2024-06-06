82f971f08bc79b1ef5e3811a6970ba6ccd534048
Anthony <anthonymwakulila@gmail.com>
2024-06-06 03:03:27 +0300

    user custom profile and manager models

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+#key classes used to customize and
+# extend the default user mode
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
+
+
+class UserProfileManager(BaseUserManager):
+    """Manager for our user profiles"""
+    def create_user(self, email, name, password=None):
+        """creating a new user profile"""
+        if not email:
+            raise ValueError('User must have a valid email')
+        #This converts the email to a normalized format (e.g., converting the domain part to lowercase).
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        #use this to ensure password is hidden in # encrypt
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """create a new superuser"""
+        user = self.create_user(email, name, password)
+
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Retrieve full name of the user"""
+        return self.name
+
+    def get_short_name(self):
+        """Retrive short name"""
+        return self.name
+
+    def __str__(self):
+        """return string rep of the user"""
+        return self.email
 # Create your models here.
